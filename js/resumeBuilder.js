9d0c4a1ff3445cbf671a2029a292229ed8894e96
Anantha Shankar Arun Kumar <anantha.shankar1990@gmail.com>
2016-03-12 03:07:27 -0500

    Commit Test

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var funThoughts1 = "This is awesome time";
+var funThoughts2 = funThoughts1.replace("awesome", "fun");
+$("#main").append(funThoughts2);
+
+
